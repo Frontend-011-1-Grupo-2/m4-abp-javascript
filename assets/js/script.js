f81c2fab888c55d726a19d922a444d70f4949351
iVerdugoDev <ivan270@gmail.com>
2026-01-29 12:43:02 -0300

    termina fase leccion 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@
   - prompt para pedirle al usuario un dato o mensaje
   - alert: para mostrar un mensaje informativo. OJO! detienen la ejecución del programa
 */
+// 1. Mensaje en consola
+console.log(
+  '%c********* Bienvenido a la aplicación en consola *********',
+  'color: white; background-color: teal; padding: 5px; border-radius: 5px',
+);
+
+// prompt('Escriba su nombre');
+
+// alert('Este mensaje detendrá la ejecución del programa');
+
+// confirm('Seguro que desea eliminar el recurso?'); // mensaje de confirmación para el usuario: Cancelar = devuelve false, Aceptar = devuelve True
+
+// Después del alert...
+// console.log('Se ve?');
+// console.log('Aló?');
